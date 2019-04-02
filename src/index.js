34c3d3e0ed7263465c56be1d2dc17944a2e965a7
usedtobemeonce <seven.eightm5@gmail.com>
2019-04-02 19:20:04 +0200

    add dotenv for heroku deploy

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+require('dotenv').config();
+
 const app = (
     <BrowserRouter>
         <App />
